709fc98861f297364667af14f56f2403e5c065ae
manitra <ranarymiaryntsoa@gmail.com>
2021-05-09 14:51:30 +0300

    ok

--- src/views/Produits.vue
+++ src/views/Produits.vue
@@ -8,7 +8,7 @@
     <!-- Content Row -->
     <div class="row">
       <!-- Earnings (Monthly) Card Example -->
-      <div class="col-xl-3 col-md-6 mb-4">
+      <div class="col-xl-3 col-md-6 mb-4" v-for="(produit, index) in produits" :key="produit.id" ::key="index">
         <div class="card border-left-primary shadow h-100 py-2">
           <div class="card-body">
             <div class="row no-gutters align-items-center">
@@ -16,37 +16,12 @@
                 <div
                   class="text-xs font-weight-bold text-primary text-uppercase mb-1"
                 >
-                  Robe de soirée
-                </div>
+{{produit.name}}                </div>
                 <div class="h5 mb-0 font-weight-bold text-gray-800">
-                  $40,000
-                </div>
+${{produit.price}}                </div>
               </div>
               <div class="col-auto">
                 <img src="../assets/robe.jpg" style="width:100%;"/>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- Earnings (Monthly) Card Example -->
-      <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-success shadow h-100 py-2">
-          <div class="card-body">
-            <div class="row no-gutters align-items-center">
-              <div class="col mr-2">
-                <div
-                  class="text-xs font-weight-bold text-success text-uppercase mb-1"
-                >
-                  Costumes
-                </div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">
-                  $215,000
-                </div>
-              </div>
-              <div class="col-auto">
-                <img src="../assets/costumes.jpg" style="width:100%;"/>
               </div>
             </div>
           </div>
@@ -59,6 +34,15 @@
 <script>
 export default {
   name: "Produits",
+  data() {
+    return {
+      modal: false,
+      produits: [
+        { name: "robe de soirée", price: 1500, img: "../assets/robe.jpg" },
+        { name: "Costume", price: 2500, img: "../assets/robe.jpg" },
+      ],
+    };
+  },
 };
 </script>
 
