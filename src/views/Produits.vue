215aca2edc04fd0aa848009fb5bb3b7b79604ec5
manitra <ranarymiaryntsoa@gmail.com>
2021-05-10 16:50:43 +0300

    okdeleteproduit

--- src/views/Produits.vue
+++ src/views/Produits.vue
@@ -3,21 +3,33 @@
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
       <h1 class="h3 mb-0 text-gray-800">LES PRODUITS</h1>
+      <a
+        href="#"
+        class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"
+        v-on:click="addproduit()"
+      >
+        Créer</a
+      >
     </div>
 
     <!-- Content Row -->
     <div class="row">
       <!-- Earnings (Monthly) Card Example -->
-      <div class="col-xl-3 col-md-6 mb-4"  v-for="(produit, index) in produits"
+      <div
+        class="col-xl-3 col-md-6 mb-4"
+        v-for="(produit, index) in produits"
         :key="produit.id"
         ::key="index"
-       >
+      >
         <div class="card border-left-primary shadow h-100 py-2">
           <div class="card-body">
             <div class="row no-gutters align-items-center">
-              <div class="col mr-2"  data-toggle="modal"
-        data-target="#exampleModal"
-        v-on:click="openModal(index)">
+              <div
+                class="col mr-2"
+                data-toggle="modal"
+                data-target="#exampleModal"
+                v-on:click="openModal(index)"
+              >
                 <div
                   class="text-xs font-weight-bold text-primary text-uppercase mb-1"
                 >
@@ -27,7 +39,25 @@
                   ${{ produit.price }}
                 </div>
               </div>
-              <button class="pull-right btn btn-secondary " v-on:click="contextuelmenu()">...</button> 
+              <div class="dropdown">
+                <button
+                  class="pull-right btn btn-secondary"
+                  id="dropdownMenuButton"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  ...
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a
+                    class="dropdown-item"
+                    href="#"
+                    v-on:click="contextuelMenu(index)"
+                    >supprimer</a
+                  >
+                </div>
+              </div>
               <div class="col-auto">
                 <img src="../assets/robe.jpg" style="width: 100%" />
               </div>
@@ -120,8 +150,15 @@ export default {
       this.produits[this.editIndex].name = this.name;
       this.produits[this.editIndex].price = this.price;
     },
-    contextuelMenu() {
-      alert("ok");
+    contextuelMenu(index) {
+      const filter = (produit, i) => {
+        if (index != i) return true;
+        else return false;
+      };
+      this.produits = this.produits.filter(filter);
+    },
+    addproduit() {
+      alert('ok')
     },
   },
 };
