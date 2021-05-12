683e8cc4e012e9d0a7aa2e4ccb72262a41e7ff49
manitra <ranarymiaryntsoa@gmail.com>
2021-05-12 16:23:07 +0300

    jsvoid

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -7,7 +7,7 @@
     <!-- Sidebar - Brand -->
     <a
       class="sidebar-brand d-flex align-items-center justify-content-center"
-      href="#"
+      href="javascript:void(0)"
     >
       <div class="sidebar-brand-icon rotate-n-15">
         <i class="fab fa-shopify"></i>
